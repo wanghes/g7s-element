78dddf223e2c93f3c539ca738706ed1094cb2fdf
wang_hes <wang_hes@sina.cn>
2018-07-30 18:36:10 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# g7s-element
-g7s-element
+## 基于 element-ui 基础构建g7s前端的组件及样式，进行第二次开组件组件，
+
+深入element组件内部按项目需求修改交互逻辑
+
+```shell
+npm install g7s-element -S
+```
+
+## 快速开始
+``` javascript
+import Vue from 'vue'
+import G7SElement from 'g7s-element'
+
+Vue.use(G7SElement)
+
+## 按需加载
+
+按需加载.balelrc设置
+```shell
+{
+  "presets": [
+    ["env", {
+      "modules": false,
+      "targets": {
+        "browsers": ["> 1%", "last 2 versions", "not ie <= 8"]
+      }
+    }],
+    "stage-2"
+  ],
+  "plugins": [
+    [
+      "component",
+      {
+        "libraryName": "g7s-element",
+        "styleLibraryName": "theme-chalk"
+      }
+    ]
+  ]
+}
+```
+
+```javascript
+import {
+  Select,
+  Button
+  // ...
+} from 'g7s-element'
+
+Vue.component(Select.name, Select)
+Vue.component(Button.name, Button)
+```
+## 单文件组件按需加载
+
+```html
+<template>
+  <div class="hello">
+    <h1>{{Button.name}}</h1>
+    <el-button>test</el-button>
+  </div>
+</template>
+
+<script>
+  import {
+    Button
+  } from 'g7s-element'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      Button: Button,
+    }
+  },
+  components: {
+    [Button.name]:Button
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+```
+
+
+## LICENSE
+[MIT](LICENSE)
---
+++ webpack.conf.js
@@ -0,0 +1,104 @@
+const path = require('path');
+const webpack = require('webpack');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: ['./element/src/index.js']
+    },
+    output: {
+        path: path.resolve(process.cwd(), './lib'),
+        publicPath: '/dist/',
+        filename: 'index.js',
+        chunkFilename: '[id].js',
+        libraryTarget: 'umd',
+        library: 'ELEMENT',
+        umdNamedDefine: true
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            main: path.resolve(__dirname, './element/src'),
+            packages: path.resolve(__dirname, './element/packages'),
+            'element-ui': path.resolve(__dirname, './element')
+        }
+    },
+    // externals: {
+    //     vue: config.vue
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.(jsx?|babel|es6)$/,
+                include: process.cwd(),
+                exclude: ['node_modules'],
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    preserveWhitespace: false
+                }
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader?minimize=false'
+            },
+            {
+                test: /\.otf|ttf|woff2?|eot(\?\S*)?$/,
+                loader: 'url-loader',
+                query: {
+                  limit: 10000,
+                  name: path.posix.join('static', '[name].[hash:7].[ext]')
+                }
+            },
+            {
+                test: /\.svg(\?\S*)?$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    name: path.posix.join('static', '[name].[hash:7].[ext]')
+                }
+            },
+            {
+                test: /\.(gif|png|jpe?g)(\?\S*)?$/,
+                loader: 'url-loader',
+                query: {
+                  limit: 10000,
+                  name: path.posix.join('static', '[name].[hash:7].[ext]')
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ProgressBarPlugin(),
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            output: {
+                comments: false
+            },
+            sourceMap: false
+        }),
+        new webpack.LoaderOptionsPlugin({
+          minimize: true
+        })
+    ]
+};
